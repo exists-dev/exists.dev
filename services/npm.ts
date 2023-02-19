304ea122bdc2c2be1e965b4bdb451b63d5e1c190
TerminalFi <Zachary.Schulze@gmail.com>
2023-02-19 15:35:10 -0600

    formatting

--- services/npm.ts
+++ services/npm.ts
@@ -3,8 +3,10 @@ import { SearchResults } from '../types/npm';
 
 
 export class NpmRegistry {
+
   async search(query: string): Promise<SearchResults> {
     const response = await axios.get(`https://registry.npmjs.com/-/v1/search?text=${query}`);
     return response.data as SearchResults;
   }
+
 }
